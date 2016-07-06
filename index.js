e4f750ae61edb83dc1f1b3034528a89cd4e8c5ac
EC2 Default User <ec2-user@ip-172-31-12-139.us-west-2.compute.internal>
2016-07-06 05:17:49 +0000

    change port back

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('babel-register');
 const server = require('./server/index').default;
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, function () {
   console.log('Server listening on', PORT);
 });
