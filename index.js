25696c25431c4d53e7b5a9933664e9e883770a81
FrankMa <FMa@bauermedia.co.nz>
2016-07-06 15:37:19 +1200

    change port to 80 for aws test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('babel-register');
 const server = require('./server/index').default;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, function () {
   console.log('Server listening on', PORT);
 });
